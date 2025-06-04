c011b1e058b0feeef8d67cb4153f074334f95429
AlickLiubc <nrlbc@sina.com>
2025-06-04 22:10:18 +0800

    first commit

---
+++ src/main/java/org/example/CpuProblemsApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpuProblemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CpuProblemsApplication.class);
+    }
+
+}
